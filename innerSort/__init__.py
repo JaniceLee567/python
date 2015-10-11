cac956056b9e2679d8dc320ac10aa832f16206ea
janice <li.yuanzhen@zte.com.cn>
2015-10-11 22:27:04 +0800

    commit straight insert sort

---
+++ innerSort/__init__.py
@@ -0,0 +1,11 @@
+# inner sort algorithm:
+
+#         insert_sort : straight_insert_sort, shell_sort
+#         
+#         select_sort : simple_select_sort, heap_sort
+# 
+#         change_sort : bubble_sort, quick_sort
+#         
+#         merge_sort
+#         
+#         radix_sort
---
+++ innerSort/straight_insert_sort.py
@@ -0,0 +1,29 @@
+# straight insert sort
+#     assuming: before key, the occurrence is order
+#     stability: stable
+#     time_complex: O(n^2)
+
+import random
+
+def straight_insert_sort(array):
+    count = len(array)
+
+    for i in range(1, count):
+        key = array[i]
+        for j in range(i-1, -1, -1):
+            if array[j] > key:
+                array[j+1] = array[j]  # former larger and backward
+                k = j   # reserve insert point
+            else:
+                k = j+1 # reserve insert point
+                break
+        array[k] = key # insert data
+        print array
+
+
+
+
+if __name__ == '__main__':
+    array = [random.randint(10, 50) for i in range(10)]
+    print array
+    straight_insert_sort(array) 
