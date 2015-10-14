9c09b98dfdaef88dcf1757bc4e650bdbfa129ec2
janice <li.yuanzhen@zte.com.cn>
2015-10-15 00:19:24 +0800

    commit heap sort /1st.
    the cord is undone.

---
+++ innerSort/heap_sort.py
@@ -0,0 +1,17 @@
+#heap_sort:
+#	description:
+#	stability:
+#	time_complex:
+
+import random
+
+def heap_sort(array):
+    pass
+
+
+if __name__ == '__main__':
+    array = [random.randint(10, 50) for i in range(10)]
+    print array
+    heap_sort(array)
+    print array
+
